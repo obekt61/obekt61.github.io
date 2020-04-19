3da324de64398c09cb8a9cc5cde3a0d72bbb6667
constairs <constairs@gmail.com>
2020-04-19 15:22:28 +0700

    change pages; add new styles

--- js/common.js
+++ js/common.js
@@ -1,14 +1,22 @@
 $(function() {
-
-
 	$('.spoiler-link').click(function(e) {
-
 		e.preventDefault();
 		var content = '#' + e.target.id + 'Content';
 		$(content).slideToggle();
-		
 	});
 
-	// Custom JS
+	document.getElementById('year').innerText = new Date().getFullYear();
 
+	var grid = document.querySelector('.grid');
+
+	var msnry = new Masonry(grid, {
+		itemSelector: '.grid-item',
+		columnWidth: '.grid-sizer',
+		percentPosition: true
+	});
+
+	imagesLoaded(grid).on('progress', function () {
+		// layout Masonry after each image loads
+		msnry.layout();
+	});
 });
